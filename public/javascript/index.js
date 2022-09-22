757750cf65cd517822fa94940d515c6149a1ad9b
Kenneth David Leonel Triana <60140555+kennethLeonel@users.noreply.github.com>
2022-09-22 17:42:16 -0500

    Se realizo dos funciones

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -4,16 +4,16 @@
 
 let categorias = [{}];
 
-// Se le pedirá al usuario que ingrese la cantidad de categorías que posee.
 
-// do {
-    let cantidadCategorias
+
+// Se le pedirá al usuario que ingrese la cantidad de categorías que posee.
+let cantidadCategorias ;
 do {
      cantidadCategorias =  parseInt(prompt("Ingrese la cantidad de categorías de gastos que posee"));
      if (cantidadCategorias > 0) {
         
         console.log(cantidadCategorias);
-    
+        
         // Se le pedirá al usuario que ingrese el nombre de cada categoría.
     
         for (let i = 0; i < cantidadCategorias; i++) {
@@ -24,7 +24,11 @@ do {
             let gasto = prompt("Ingrese el gasto de la categoría " + nombreCategoria);
             //Validando que el gasto sea un número si lo es agrego la categoría al array
             if (Number(gasto)) {
-                categorias[i] = {nombre: nombreCategoria, gasto: parseInt(gasto)};
+                categorias[i] = {
+                    id: i,
+                    nombre: nombreCategoria,
+                    gasto: parseInt(gasto)
+                };
                 alert("Categoría "+nombreCategoria +" agregada correctamente");
             }
             else {
@@ -46,6 +50,47 @@ do {
         alert("La cantidad de categorías debe ser un número mayor a 0");
     }
 } while (isNaN(cantidadCategorias) || cantidadCategorias < 1);
+
+const eliminar = (id) => {
+    console.log("Entre a eliminar");
+    let categoriasFiltradas = categorias.filter( cate => cate.id !==id);
+    console.log("Se eliminó la categoría con id: "+id);
+    console.log(categoriasFiltradas);
+    let parrafo = document.getElementById("objeto");
+    parrafo.innerHTML = JSON.stringify(categoriasFiltradas);
+  
+};  
+
+const totalGastos = (arreglo) => {
+    let total = 0;
+    arreglo.forEach(item => {
+        total += item.gasto;
+    });
+    return total;
+};
+
+
+setTimeout(() => {
+
+    let hola = prompt("Desea eliminar alguna categoría? (si/no)");
+    if (hola == "si") {
+        let id = parseInt(prompt("Ingrese el id de la categoría que desea eliminar"));
+        eliminar(parseInt(id));
+    }else if (hola == "no") {
+        alert ("El total de gastos que posee es: "+totalGastos(categorias));
+        alert("Gracias por usar nuestro programa");
+        
+    } 
+        
+    }, 3000);
+
+
+
+
+
+
+
+
 
 
 
